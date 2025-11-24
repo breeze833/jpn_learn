1bf41e2fe3e83ac3f71b08aea992b2e70ab40365
Feng-Cheng Chang <breeze833@gmail.com>
2025-11-24 22:15:22 +0800

    Rev: Change comment to English

--- jpn_learn_app/jpn_learn_sw.js
+++ jpn_learn_app/jpn_learn_sw.js
@@ -1,5 +1,5 @@
-// 最簡單的 Service Worker，什麼都不做，只為了滿足 PWA 安裝條件
+// The simplest Service Worker. It does nothing, only for satifying PWA specification
 self.addEventListener('fetch', function(event) {
-  // 這裡可以實作快取邏輯，但為了安裝，留空或直接 pass 請求也可以
+  // can do something such as caching
   return; 
 });
